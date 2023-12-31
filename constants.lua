b25055d4c31fa03ca90e7f2bd5224579bcb8a31f
Fethanf <ethan.falik.choochoo@gmail.com>
2023-12-30 20:09:29 -0600

    constants file

---
+++ constants.lua
@@ -0,0 +1,17 @@
+-- the lowest point the ufo can go
+LOW_LIMIT = 80
+
+
+--half the width of the abduction beam
+BEAM_HALF_WIDTH = 8
+
+--centre of player x and y offset
+CENTRE_OF_PLAYER_OFFSET_X = 4
+CENTRE_OF_PLAYER_OFFSET_Y = 7
+
+--Buttons codes--
+BTN_X = 5
+
+--Spawn Rates (seconds)--
+SPAWN_RATE_COW = 10
+SPAWN_RATE_FARMER = 5
